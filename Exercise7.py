b2ba7cb7916a09cb7429e15d671c64029d35d74d
asryanani1997 <asryan.ani1997@gmail.com>
2023-03-17 02:15:27 +0400

    Added Exercise7 File

---
+++ Exercise7.py
@@ -0,0 +1,5 @@
+"""Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers."""
+x=24
+if x%2==0: print ("even")
+if x%2==1: print ("odd")
+
